ebab4348bdad3ed1d7cfa34a47ccb64d248d274d
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-06 22:02:13 +0530

    move somee functions to controllers create Shop js controllers

--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,9 @@
 const path = require('path');
 
 const express = require('express');
-
-const rootDir = require('../util/path');
-const adminData = require('./admin');
-
+const productsController = require('../controller/Products');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', productsController.getProduct);
 
 module.exports = router;
