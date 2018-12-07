f482ef4cd7e42fd49bf3b9831889d6f5d7f745d4
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-07 11:29:07 +0530

    Add New Markup fix the old MarkUp

--- routes/admin.js
+++ routes/admin.js
@@ -2,18 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controller/Products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.getAddProduct);
-
-// exports.routes = router;
-//exports.products = products;
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 const express = require('express');
-const productsController = require('../controller/Products');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-router.get('/', productsController.getProduct);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
