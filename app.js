7f99adc76398f6499807f5e75c747e5b6aa12b1c
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-07 13:07:57 +0530

    create connection and return Promises and log data

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
+db.execute('select * from products')
+    .then((result) => {
+        console.log(result[0][1]);
 
+    })
+    .catch((err) => {
+        console.log
+    })
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,7 +21,9 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
