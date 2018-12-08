6ddb68933319735c8f225f14bae3c08a81e6185a
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-08 21:54:26 +0530

    display Product By Id

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,7 +14,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then(() => {
+    .then((product) => {
+      res.render('shop/product-detail', {
+        prods: product,
+        pageTitle: product.title,
+        path: '/products'
+      })
 
     })
     .catch((err) => {
