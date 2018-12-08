14e8ec12c25ce76c3424c0a760eb64e4bcb482b7
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-08 18:59:19 +0530

    Find Data By Id

--- models/product.js
+++ models/product.js
@@ -25,7 +25,7 @@ module.exports = class Product {
 
   }
 
-  static findById(id, cb) {
-
+  static findById(id) {
+    return db.execute('select * from products where products.id = ?', [id]);
   }
 };
