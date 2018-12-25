e526d2b6d5ebab0823edc8cdd0d15222481e91e7
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-25 18:58:10 +0530

    User populate to display reletionship between two models

--- controllers/admin.js
+++ controllers/admin.js
@@ -79,8 +79,10 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find({})
+  Product.find()
+    // .populate('userId')
     .then(products => {
+      console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
