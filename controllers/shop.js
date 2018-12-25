e4aac9ef4d424261226d5e477e38d4e5147a20c8
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-25 17:38:44 +0530

    show all items Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,10 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find({})
     .then(products => {
+      console.log(products);
+
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
@@ -37,7 +39,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find({})
     .then(products => {
       res.render('shop/index', {
         prods: products,
