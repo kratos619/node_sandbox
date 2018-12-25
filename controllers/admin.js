e44d62cdbbe26ac4c713886d505b42f9bf637b6d
kratos619 <gaurav.palaspagar@hotmail.com>
2018-12-25 18:44:15 +0530

    Add UserId to product by req.user or req.user.id

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,7 +17,8 @@ exports.postAddProduct = (req, res, next) => {
     title: title,
     imageUrl: imageUrl,
     price: price,
-    description: description
+    description: description,
+    userId: req.user
   });
   product
     .save()
