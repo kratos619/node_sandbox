127550986892a739650e44324c89ae3c10aa8554
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-21 09:39:31 +0530

    run app on port

--- mean_stack/node_backend/app.js
+++ mean_stack/node_backend/app.js
@@ -1,2 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
+const PORT = process.env.APP_PORT || 
+
+
+app.listen(process.env.APP_PORT,() => {
+    console.log("app is running on port", process.env.APP_PORT)
+});
