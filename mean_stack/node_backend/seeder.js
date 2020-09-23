f2c87dacbd9add462f4bad88a46699d08d7f1bf1
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-23 13:34:05 +0530

    complete bootcamp seeder

--- mean_stack/node_backend/seeder.js
+++ mean_stack/node_backend/seeder.js
@@ -14,3 +14,31 @@ mongoose.connect(process.env.MONGO_CONNECTION_URI, {
 const bootCamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
+
+//import data in db
+const importData = async () => {
+  try {
+    await BootCamp.create(bootCamps);
+    console.log("data Imported...");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// delete data
+const deleteData = async () => {
+  try {
+    await BootCamp.deleteMany();
+    console.log("data Dstroyed...");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] == "-i") {
+  importData();
+} else if (process.argv[2] == "-d") {
+  deleteData();
+}
