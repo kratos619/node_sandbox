e8ee3ff388658ff4afad8462fbb0a6117a9bbcb2
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-28 19:46:24 +0530

    :fire:  add validation

--- node-js-rest-api/routes/user.route.js
+++ node-js-rest-api/routes/user.route.js
@@ -2,10 +2,21 @@ const express = require("express");
 const router = express.Router();
 const { createUser } = require("../controller/UserController");
 const { check } = require("express-validator/check");
+const users = require("../model/Users.model");
 router.post(
   "/user/create",
-  check("email", "email required").isEmail(),
-  check("password", "password field required").notEmpty(),
+  [
+    check("email", "email required")
+      .isEmail()
+      .custom((value, { req }) => {
+        return users.findOne({ email: value }).then((res) => {
+          if (res) {
+            return Promise.reject("Email alreay taken pick diffrent one");
+          }
+        });
+      }),
+    check("password", "password field required").notEmpty(),
+  ],
   createUser
 );
 
